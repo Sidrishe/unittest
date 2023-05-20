7aae6354e070f2a993feb14364a35aa2ff7773ca
Mikhail <osipov.m93@mail.ru>
2023-05-20 17:59:29 +0300

    add tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -11,3 +11,11 @@ class TestArrs(unittest.TestCase):
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        """Проверка с пустым массивом"""
+        self.assertEqual(arrs.my_slice([], 0, 2), [])
+        """Проверка с отрицательным start и end None"""
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], -3), [2, 3, 4])
+        """Проверка со start=0 и end None"""
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 0), [1, 2, 3, 4])
+        """проверка если старт меньше -длинны строки"""
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4, 5], -10, 2), [1, 2])
